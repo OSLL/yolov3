3748517490104224c1eca47523c59fbce3253965
light5551 <sergey.glazunov.99@mail.ru>
2022-12-05 01:16:56 +0300

    minir

--- export_torch_script.sh
+++ export_torch_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 export.py --weights yolov3-tiny.pt --include torchscript
+# python3 export.py --weights yolov3-tiny.pt --include torchscript
+python3 export.py --weights yolov3-tiny.pt  --dynamic --include onnx
